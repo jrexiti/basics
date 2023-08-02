961c75f332dd4400b0590a6beae4eb59bfa7525d
jrexiti <jay.rexiti@gmail.com>
2023-08-01 22:20:41 -0400

    intro data types

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 int main()
 {
-    std::cout << "hello world!";
+
+    int number = 100, anotherNumber = 200;
+    double price = 9.99;
+
+    const double pi = 3.14;
+    std::cout << "hello world" << std::endl;
+    std::cout << number << std::endl;
+    std::cout << price << std::endl;
+
+    std::cout << 2 * pi << std::endl;
+
     return 0;
 }
