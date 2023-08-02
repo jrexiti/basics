8597f1af38ddeb2e1c1519c43b68e5c5a818ea78
jrexiti <jay.rexiti@gmail.com>
2023-08-02 13:59:37 -0400

    updated to min/max

--- get_min_max.cpp
+++ get_min_max.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int get_max_val(vector<int> num_list)
 {
 
+    cout << "coming from looped max func " << endl;
+
     int max_val = num_list.at(0);
     for (int i = 1; i < num_list.size(); i++)
     {
@@ -18,12 +20,45 @@ int get_max_val(vector<int> num_list)
 
 int get_min_val(vector<int> num_list)
 {
+
+    cout << "coming from looped min func " << endl;
+
     int min_val = num_list.at(0);
     for (int i = 1; i < num_list.size(); i++)
     {
         if (num_list.at(i) < min_val)
         {
             min_val = num_list.at(i);
+        }
+    }
+    return min_val;
+}
+
+int get_max_val_range_based(vector<int> num_list)
+{
+    cout << "coming from ranged max func " << endl;
+
+    int max_val = num_list.at(0);
+    for (int num : num_list)
+    {
+        if (num > max_val)
+        {
+            max_val = num;
+        }
+    }
+    return max_val;
+}
+
+int get_min_val_range_based(vector<int> num_list)
+{
+    cout << "coming from ranged min func " << endl;
+
+    int min_val = num_list.at(0);
+    for (int num : num_list)
+    {
+        if (num < min_val)
+        {
+            min_val = num;
         }
     }
     return min_val;
@@ -40,6 +75,8 @@ int main()
 
     cout << get_max_val(num_list) << endl;
     cout << get_min_val(num_list) << endl;
+    cout << get_max_val_range_based(num_list) << endl;
+    cout << get_min_val_range_based(num_list) << endl;
 
     return 0;
 }
