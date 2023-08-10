ea0cd505fc1404c0915de736d297a41e382656fa
jrexiti <jay.rexiti@gmail.com>
2023-08-10 09:57:00 -0400

    pointer basics

--- pointers.cpp
+++ pointers.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
+
+    int n = 10;
+    int *p = &n;
+
+    cout << "Address of n: " << &n << endl;
+    cout << "Value of n: " << n << endl;
+    cout << "Value of p: " << p << endl;
+
+    cout << "-------------------" << endl;
+
     return 0;
 }
