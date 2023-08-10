5c397806cb1fd4d7f5ab1d5b515f49e26eef49d9
jrexiti <jay.rexiti@gmail.com>
2023-08-10 10:02:24 -0400

    derefrence pointer

--- pointers.cpp
+++ pointers.cpp
@@ -10,6 +10,8 @@ int main()
     cout << "Address of n: " << &n << endl;
     cout << "Value of n: " << n << endl;
     cout << "Value of p: " << p << endl;
+    cout << "Value of *p: " << *p << " (dereferencing p)"
+         << "  *p = value of whatever p is pointing to (using the '*' dereference operator)" << endl;
 
     cout << "-------------------" << endl;
 
